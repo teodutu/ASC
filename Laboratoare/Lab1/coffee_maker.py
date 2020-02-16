dc6c23772d290d10372d3985237aaec4991ffd10
Teodor Dutu <teodor.dutu@gmail.com>
2020-02-16 17:03:13 +0200

    [Lab 1] Adaugare make help list + bonus

--- Laboratoare/Lab1/coffee_maker.py
+++ Laboratoare/Lab1/coffee_maker.py
@@ -1,50 +1,94 @@
-#! /usr/bin/python
+#! /opt/anaconda3/bin/python3
 
 """
 A command-line controlled coffee maker.
 """
 
 import sys
+import load_recipes as recipes
 
-"""
-Implement the coffee maker's commands. Interact with the user via stdin and print to stdout.
+# Implement the coffee maker's commands. Interact with the user via stdin and print to stdout.
 
-Requirements:
-    - use functions
-    - use __main__ code block
-    - access and modify dicts and/or lists
-    - use at least once some string formatting (e.g. functions such as strip(), lower(),
-    format()) and types of printing (e.g. "%s %s" % tuple(["a", "b"]) prints "a b"
-    - BONUS: read the coffee recipes from a file, put the file-handling code in another module
-    and import it (see the recipes/ folder)
+# Requirements:
+#    - use functions
+#    - use __main__ code block
+#    - access and modify dicts and/or lists
+#    - use at least once some string formatting (e.g. functions such as strip(), lower(),
+#    format()) and types of printing (e.g. "%s %s" % tuple(["a", "b"]) prints "a b"
+#    - BONUS: read the coffee recipes from a file, put the file-handling code in another module
+#    and import it (see the recipes/ folder)
 
-There's a section in the lab with syntax and examples for each requirement.
+# There's a section in the lab with syntax and examples for each requirement.
 
-Feel free to define more commands, other coffee types, more resources if you'd like and have time.
-"""
+# Feel free to define more commands, other coffee types, more resources if you'd like and have time.
 
-"""
-Tips:
-*  Start by showing a message to the user to enter a command, remove our initial messages
-*  Keep types of available coffees in a data structure such as a list or dict
-e.g. a dict with coffee name as a key and another dict with resource mappings (resource:percent)
-as value
-"""
+# Tips:
+# *  Start by showing a message to the user to enter a command, remove our initial messages
+# *  Keep types of available coffees in a data structure such as a list or dict
+# e.g. a dict with coffee name as a key and another dict with resource mappings (resource:percent)
+#as value
 
 # Functii
-def exit_coffee_maker(command):
+def exit_coffee_maker():
+    """Retruneaza True ca sa incheie loopul din run_commands."""
+
     return True
 
-def list_coffees(command):
-    pass
+def list_coffees():
+    """Listeaza toate tipurile de cafele pe care aparatul le poate face."""
+    length = len(COFFEE_TYPES)
 
-def make_coffee(command):
-    pass
+    for i in range(length):
+        if i == length - 1:
+            print("%s" % COFFEE_TYPES[i])
+        else:
+            print(str(COFFEE_TYPES[i]) + ", ", end="")
 
-def coffee_help(command):
-    pass
+def make_coffee():
+    """
+    Citeste de la stdin un tip de cafea, dupa care obtine ingredientele necesare.
 
-def refill(command):
+    In cazul in care tipul de cafea este valid si sunt disponibile destule
+    ingrediente, scade cantitatile de ingrediente necesare prepararii cafelei din
+    cele totale.
+    In caz contrar, afiseaza mesaje de eroare corespunzatoare.
+    """
+    print("Which type?")
+
+    coffee_type = sys.stdin.readline().strip("\n")
+    used_ingredients = recipes.get_recipe(coffee_type)
+
+    if not bool(used_ingredients):
+        return
+
+    can_make_coffee = True
+
+    for ingredient in RESOURCES:
+        if RESOURCES[ingredient] < used_ingredients[ingredient]:
+            can_make_coffee = False
+            print("Not enough %s!" % ingredient)
+            break
+
+    if can_make_coffee:
+        for ingredient in RESOURCES:
+            RESOURCES[ingredient] -= used_ingredients[ingredient]
+
+        print("Here's your %s!" % coffee_type)
+
+def coffee_help():
+    """Listeaza comenzile suportate impreuna cu scurte descrieri ale acestora."""
+    print("Available commands:\n" +
+          "'list' - lists available coffee types\n" +
+          "'status' - prints the available amount of each ingredient\n" +
+          "'refill' - brings the amount of one or all of the ingredients to 100%\n" +
+          "'make' - prepares a certain type of coffe\n" +
+          "'exit' - exits the coffee maker")
+
+def refill():
+    """
+    Aduce cantitatea dintr-un anumit ingredient la FULL, sau pe cea a tuturor
+    ingredientelor in caz ca se specifica 'all'.
+    """
     print("Which resource? Type 'all' for refilling everything")
 
     resource = sys.stdin.readline().strip("\n")
@@ -52,23 +96,28 @@ def refill(command):
     if resource in RESOURCES:
         RESOURCES[resource] = FULL
     elif resource == ALL:
-        for key in RESOURCES.keys():
+        for key in RESOURCES:
             RESOURCES[key] = FULL
 
-def status(command):
+def status():
+    """Afiseaza cantitatile curente de ingrediente."""
     for ingredient, amount in RESOURCES.items():
         print("%s: %d%%" % (ingredient, amount))
 
 def run_commands():
+    """
+    Citeste comenzi de la tastatura si apeleaza functiile corespunzatoare
+    pana se citeste comanda exit.
+    """
     should_close = None
 
-    while should_close == None:
-        print("Enter command")
+    while should_close is None:
+        print("\nEnter command")
 
         command = sys.stdin.readline().strip("\n")
 
         if command in COMMANDS:
-            should_close = COMMANDS[command](command)
+            should_close = COMMANDS[command]()
         else:
             print("Unknown command!")
 
@@ -129,6 +178,8 @@ exit
 """
 
 def main():
+    """Prints an initial string then calls run_commands."""
+
     print("I'm a smart coffee maker")
     run_commands()
 
--- Laboratoare/Lab1/load_recipes.py
+++ Laboratoare/Lab1/load_recipes.py
@@ -1,13 +1,39 @@
 """
-	Bonus task: load all the available coffee recipes from the folder 'recipes/'
-	File format:
-		first line: coffee name
-		next lines: resource=percentage
+    Bonus task: load all the available coffee recipes from the folder 'recipes/'
+    File format:
+        first line: coffee name
+        next lines: resource=percentage
 
-	info and examples for handling files:
-		http://cs.curs.pub.ro/wiki/asc/asc:lab1:index#operatii_cu_fisiere
-		https://docs.python.org/3/library/io.html
-		https://docs.python.org/3/library/os.path.html
+    info and examples for handling files:
+        http://cs.curs.pub.ro/wiki/asc/asc:lab1:index#operatii_cu_fisiere
+        https://docs.python.org/3/library/io.html
+        https://docs.python.org/3/library/os.path.html
 """
 
 RECIPES_FOLDER = "recipes/"
+
+def get_recipe(coffee_type):
+    """
+    Citeste cantitatile necesare de ingrediente din fiserul aferent tipului de
+    cafea primit ca parametru.
+    Daca fisierul exista, iar fisierul contine informatie pentru cafeaua dorita,
+    returneaza un dictionar ce contine cantitatile folosite din fiecare
+    ingredient.
+    """
+    file_name = RECIPES_FOLDER + coffee_type + ".txt"
+    used_ingredients = {}
+
+    try:
+        file = open(file_name, "r")
+        coffee_name = file.readline().strip('\n')
+
+        if coffee_name == coffee_type:
+            for line in file:
+                tokens = line.split('=')
+                used_ingredients[tokens[0]] = int(tokens[1])
+
+        file.close()
+    except FileNotFoundError:
+        print("File %s does not exist!" % file_name)
+
+    return used_ingredients
