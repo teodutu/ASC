bac2a10fa99d5d3de6851ce2bf892785cb686e1c
Teodor Dutu <teodor.dutu@gmail.com>
2020-04-16 23:53:54 +0300

    [Tema 2][opt_m] Adaugare comentarii despre pointeri

--- Teme/Tema2/solver_opt.c
+++ Teme/Tema2/solver_opt.c
@@ -41,14 +41,14 @@ double* my_solver(int N, double *A, double* B)
 
 	/* Se transpun matricele A: A_t = A^t si B: B_t = B^t */
 	for (register int i = 0; i != N; ++i) {
-		register double *A_t_ptr = A_t + i;
-		register double *B_t_ptr = B_t + i;
+		register double *A_t_ptr = A_t + i;  /* coloana i din A' */
+		register double *B_t_ptr = B_t + i;  /* coloana i din B' */
 
-		register double *A_ptr = A + i * N;
-		register double *B_ptr = B + i * N;
+		register double *A_ptr = A + i * N;  /* linia i din A */
+		register double *B_ptr = B + i * N;  /* linia i din B */
 
-		for (register int j = 0; j != N; ++j, A_t_ptr += N, B_t_ptr += N,
-			++A_ptr, ++B_ptr) {
+		for (register int j = 0; j != N; ++j, A_t_ptr += N,
+			B_t_ptr += N, ++A_ptr, ++B_ptr) {
 			*A_t_ptr = *A_ptr;
 			*B_t_ptr = *B_ptr;
 		}
@@ -59,11 +59,14 @@ double* my_solver(int N, double *A, double* B)
 	 * A_t = A parcurs pe linii
 	 */
 	for (register int i = 0; i != N; ++i) {
-		register double *C_ptr = C + i * N;
-		register double *B_copy = B + i * N;
+		register double *C_ptr = C + i * N;  /* linia i din C */
+		register double *B_copy = B + i * N;  /* linia i din B */
 
 		for (register int j = 0; j != N; ++j, ++C_ptr) {
+			/* rezultatul calulului pe o linie */
 			register double result = 0;
+
+			/* calculul sumei incepe de la indicele j al coloanei */
 			register double *B_ptr = B_copy + j;
 			register double *A_ptr = A + j * (N + 1);
 
@@ -79,12 +82,18 @@ double* my_solver(int N, double *A, double* B)
 	 * Se va folosi A_t parcursa pe linii
 	 */
 	for (register int i = 0; i != N; ++i) {
+		/* linia i din A^2 */
 		register double *AA_ptr = AA + i * (N + 1);
-		register double *A_copy = A + i * N;
+		register double *A_copy = A + i * N;  /* linia i din A */
 
 		for (register int j = i; j != N; ++j, ++AA_ptr) {
+			/* rezultatul calulului pe o linie */
 			register double result = 0;
+
+			/* calculul sumei incepe de la coloana i */
 			register double *A_ptr = A_copy + i;
+
+			/* linia j si coloana i din A' */ 
 			register double *A_t_ptr = A_t + j * N + i;
 
 			for (register int k = i; k <= j; ++k, ++A_ptr,
@@ -100,12 +109,17 @@ double* my_solver(int N, double *A, double* B)
 	 * Se foloseste B_t parcurs pe linii
 	 */
 	for (register int i = 0; i != N; ++i) {
-		register double *C_ptr = C + i * N;
-		register double *AA_copy = AA + i * N;
+		register double *C_ptr = C + i * N;  /* linia i din C */
+		register double *AA_copy = AA + i * N;  /* linia i din A^2 */
 
 		for (register int j = 0; j != N; ++j, ++C_ptr) {
+			/* rezultatul calulului pe o linie */
 			register double result = 0;
+
+			/* calculul sumei incepe de la coloana i */
 			register double *AA_ptr = AA_copy + i;
+
+			/* linia j si coloana i din B' */ 
 			register double *B_t_ptr = B_t + j * N + i;
 
 			for (register int k = i; k != N; ++k, ++AA_ptr,
